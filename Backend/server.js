948572e8f70888b81bbe65557bf79bddf69bf94b
Amresh Yadav <amreshky998@gmail.com>
2024-09-14 22:39:48 +0530

    just server changes

--- Backend/server.js
+++ Backend/server.js
@@ -5,5 +5,5 @@ app.get("/",(req,res)=>{
 })
 
 app.listen(process.env.PORT,()=>{
-    console.log(`server is running at port ${process.env.PORT}`);
+    console.log(`Server started at ${process.env.PORT}`);
 })
